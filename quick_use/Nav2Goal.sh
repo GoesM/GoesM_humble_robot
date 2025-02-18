7c05ba9af598c29358c96b3170aea2ae196ead69
buaa-iov-hacker-ubuntu <buaa-iov-hacker-ubuntu@buaa.edu.cn>
2025-02-18 16:07:49 +0800

    quick-use

---
+++ quick_use/Nav2Goal.sh
@@ -0,0 +1,17 @@
+ros2 action send_goal /navigate_to_pose nav2_msgs/action/NavigateToPose "     
+pose:
+  header:
+    frame_id: map
+    stamp:
+      nanosec: 642612036
+      sec: 1711025909
+  pose:
+    orientation:
+      w: 0.999989755090415
+      x: 0.0
+      y: 0.0
+      z: -0.004526556551269358
+    position:
+      x: -1.6375017166137695
+      y: -0.478118896484375
+      z: 0.0 "
---
+++ quick_use/goal_pose.sh
@@ -0,0 +1,16 @@
+ros2 topic pub /goal_pose geometry_msgs/msg/PoseStamped "
+header:
+  stamp:
+    sec: 1739865520
+    nanosec: 52503172
+  frame_id: 
+pose:
+  position:
+    x: -1.1716904640197754
+    y: -0.39624547958374023
+    z: 0.0
+  orientation:
+    x: 0.0
+    y: 0.0
+    z: -0.9832301911181993
+    w: 0.18236883306549245" -1
